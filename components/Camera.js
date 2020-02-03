1739ac89a16c107440c0b1cc160f988e6e861071
Russell Kerber <russell.kerber@gmail.com>
2020-02-02 20:20:23 -0600

    negligible change. signing off for the day

--- components/Camera.js
+++ components/Camera.js
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
 const Camera = props => {
+
+    toLogin = () => {
+        props.history.push('/');
+    }
+
     return (
         <>
         <Text>Camera page</Text>
+        <Button title="Back to Login" onPress={toLogin}></Button>
         </>
     )
 }
