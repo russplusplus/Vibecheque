49b46a380946cb801d2920c915830629e1e3e6c1
Russell Kerber <russell.kerber@gmail.com>
2020-02-03 13:53:52 -0600

    express server runs

---
+++ server/routes/pics.router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Server works!')
+})
+
+module.exports = router;
