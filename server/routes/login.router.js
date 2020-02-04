b829330251e0359df9f69ff49f9bb83f3a6e0ba2
Russell Kerber <russell.kerber@gmail.com>
2020-02-03 22:14:52 -0600

    minimal changes

---
+++ server/routes/login.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+
+router.use(bodyParser.json());
+
+router.post('/', (req, res) => {
+    console.log('req.body:', req.body);
+    const user = req.body.username;
+    res.status(200).send(`User's name is ${user}`);
+})
+
+module.exports = router;
